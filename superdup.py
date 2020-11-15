7478ffc1f53d9525fa59b6b2d03cdb0f691c292f
Kilian Lackhove <kilian@lackhove.de>
2020-11-15 19:50:37 +0100

    implemented missing functionalitíes

--- superdup.py
+++ superdup.py
@@ -6,88 +6,88 @@ import argparse
 import json
 import logging
 import re
+import smtplib
+import socket
+import ssl
 import subprocess
 import sys
 from configparser import ConfigParser
-from dataclasses import dataclass
 from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from functools import wraps
 from pathlib import Path
 from typing import List
+
+import attr
 
 logger = logging.getLogger("superdup")
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
-
-@dataclass
+@attr.s
 class Config:
-    duplicacy_command: str = "/usr/bin/duplicacy"
-    stamp_path: Path = Path("~/.duplicacy_stamps_new").expanduser().resolve()
-    log_path: Path = Path("~/.duplicacy_logs").expanduser().resolve()
-    source_path_dirs: Path = Path("/source_dirs/")
-    dry_run: bool = True
-    num_logfiles: int = 5
+    duplicacy_command: str = attr.ib(default="/usr/local/bin/duplicacy", converter=str)
+    stamp_path: Path = attr.ib(
+        default=Path("/superdup/stamps"),
+        converter=lambda x: Path(x).expanduser().resolve(),
+    )
+    log_path: Path = attr.ib(
+        default=Path("/superdup/logs"),
+        converter=lambda x: Path(x).expanduser().resolve(),
+    )
+    source_path_dirs: Path = attr.ib(
+        default=Path("/source_dirs/"),
+        converter=lambda x: Path(x).expanduser().resolve(),
+    )
+    dry_run: bool = attr.ib(default=True, converter=bool)
+    num_logfiles: int = attr.ib(default=5, converter=int)
 
-    duplicacy_env = {
-        "DUPLICACY_PASSWORD": "topsecret",
-        "DUPLICACY_B2_ID": "secret",
-        "DUPLICACY_B2_KEY": "supersecret",
-    }
+    email_to: str = attr.ib(default="bar@foo.com", converter=str)
+    email_from: str = attr.ib(default="noreply@foo.com", converter=str)
+    email_server: str = attr.ib(default="smtp.foobar.com", converter=str)
+    email_port: int = attr.ib(default=465, converter=int)
+    email_username: str = attr.ib(default="noreply@foo.com", converter=str)
+    email_password: str = attr.ib(default="verysecret", converter=str)
 
-    __instance = None
+    duplicacy_env = attr.ib(factory=dict)
 
-    def __new__(cls):
-        if cls.__instance is None:
-            cls.__instance = super(Config, cls).__new__(cls)
-        return cls.__instance
-
-    def from_ini_file(self, path: Path):
-        parser = ConfigParser()
+    @classmethod
+    def from_ini_file(cls, path: Path):
+        parser = ConfigParser(interpolation=None)
         # enable case sensitive keys
         parser.optionxform = lambda option: option
         parser.read(path)
         kwargs = dict(parser["superdup"])
-        # TODO: use attrs so we can have converters
-        self.duplicacy_command = str(
-            kwargs.get("duplicacy_command", self.duplicacy_command)
-        )
-        self.stamp_path = (
-            Path(kwargs.get("stamp_path", self.stamp_path)).expanduser().resolve()
-        )
-        self.log_path = (
-            Path(kwargs.get("log_path", self.log_path)).expanduser().resolve()
-        )
-        self.source_path_dirs = (
-            Path(kwargs.get("source_path_dirs", self.source_path_dirs))
-            .expanduser()
-            .resolve()
-        )
+        kwargs["duplicacy_env"] = dict(parser["dulicacy-env"])
 
-        envs = dict(parser["dulicacy-env"])
-        self.duplicacy_env.update(envs)
+        return cls(**kwargs)
+
+
+config = Config()
 
 
 def log_to_file(func):
     @wraps(func)
-    def wrapper_log_to_file(*args, **kwargs):
-        logfile_name = func.__name__
+    def wrapper_log_to_file(source_dir):
         logfile_path = (
-            Config().log_path / f"{logfile_name}_{datetime.now().isoformat()}.log"
+            config.log_path
+            / source_dir.name
+            / f"{func.__name__}_{datetime.now().isoformat()}.log"
         )
         logfile_path.parent.mkdir(parents=True, exist_ok=True)
 
         old_logfiles = sorted(
-            logfile_path.parent.glob(f"{logfile_name}_*.log"), reverse=True
+            logfile_path.parent.glob(f"{func.__name__}_*.log"), reverse=True
         )
-        for del_file in old_logfiles[Config().num_logfiles :]:
+        for del_file in old_logfiles[config.num_logfiles :]:
             logger.debug(f"purging old logfile {del_file}")
             del_file.unlink()
 
         file_handler = logging.FileHandler(logfile_path)
         logger.addHandler(file_handler)
 
-        value = func(*args, **kwargs)
+        value = func(source_dir)
 
         logger.removeHandler(file_handler)
 
@@ -101,12 +101,14 @@ class NetworkError(Exception):
 
 
 def test_online():
-    # TODO: implement this
-    return True
+    try:
+        socket.gethostbyname("www.google.de")
+        return True
+    except socket.error:
+        return False
 
 
 def call_duplicacy(command: List[str], cwd: Path, dry_run=None):
-    config = Config()
     dry_run = config.dry_run if dry_run is None else dry_run
 
     command.insert(0, config.duplicacy_command)
@@ -146,10 +148,11 @@ def backup(source_dir: Path) -> bool:
     logger.info(f"starting backup for {source_dir}")
     try:
         call_duplicacy(["backup", "-stats"], cwd=source_dir)
+        return True
     except subprocess.CalledProcessError:
         logger.error("backup failed for {}".format(source_dir))
         return False
-    return True
+
 
 @log_to_file
 def prune(source_dir):
@@ -169,15 +172,13 @@ def prune(source_dir):
             ],
             cwd=source_dir,
         )
+        return True
     except subprocess.CalledProcessError:
         logger.error(f"prune failed for {source_dir}")
         return False
-    return True
 
 
 def load_stamps():
-    config = Config()
-
     try:
         with open(config.stamp_path, "r") as f:
             stamps = json.load(f)
@@ -187,13 +188,12 @@ def load_stamps():
 
 
 def save_stamp(source_dir):
-    config = Config()
-
     stamps = load_stamps()
     stamps[source_dir.as_posix()] = datetime.now().isoformat()
     config.stamp_path.parent.mkdir(parents=True, exist_ok=True)
     with open(config.stamp_path, "w") as f:
         json.dump(stamps, f)
+
 
 @log_to_file
 def verify(source_dir):
@@ -208,25 +208,23 @@ def verify(source_dir):
         call_duplicacy(
             ["check", "-chunks", "-r", latest_rev, "-id", snapshot_id], cwd=source_dir,
         )
+        save_stamp(source_dir)
+        return True
     except subprocess.CalledProcessError:
         logger.error(f"verification failed for {source_dir}")
         return False
 
-    save_stamp(source_dir)
-
-    return True
 
 @log_to_file
 def check(source_dir):
     logger.info(f"starting check for {source_dir}")
-    config = Config()
 
     try:
         call_duplicacy([config.duplicacy_command, "check"], cwd=source_dir)
+        return True
     except subprocess.CalledProcessError:
         logger.error(f"ERROR: check failed for {source_dir}")
         return False
-    return True
 
 
 def is_verification_scheduled(source_dir: Path):
@@ -242,6 +240,49 @@ def is_verification_scheduled(source_dir: Path):
         return True
 
     return False
+
+
+def latest_logs(summary):
+    for source_dir, results in summary.items():
+        for step_name in results:
+            logfiles = sorted(
+                Path(config.log_path, source_dir.name).glob(f"{step_name}_*.log"),
+                reverse=True,
+            )
+            if len(logfiles) > 0:
+                yield logfiles[0]
+
+
+def email_notify(summary):
+    message = MIMEMultipart()
+    message["From"] = config.email_from
+    message["To"] = config.email_to
+    message["Subject"] = "duplicity: " + ("SUCCESS" if successful(summary) else "ERROR")
+    message.attach(MIMEText(summary_to_str(summary), "plain"))
+
+    for logfile_path in latest_logs(summary):
+        with open(logfile_path, "r") as fd:
+            attachment = MIMEText(fd.read())
+            attachment.add_header(
+                "Content-Disposition", "attachment", filename=logfile_path.name
+            )
+            message.attach(attachment)
+
+    with smtplib.SMTP_SSL(
+        config.email_server, config.email_port, context=ssl.create_default_context(),
+    ) as server:
+        server.login(config.email_username, config.email_password)
+        server.sendmail(config.email_from, config.email_to, message.as_string())
+
+    logger.info(f"sent email notification to {config.email_to}")
+
+
+def successful(summary):
+    for source_dir, results in summary.items():
+        for step_name, state in results.items():
+            if state is False:
+                return False
+    return True
 
 
 def main():
@@ -269,16 +310,18 @@ def main():
         }.get(args.verbosity)
     )
 
-    config = Config()
-    config.from_ini_file(args.config)
-    config.dry_run = args.dry_run
+    global config
+    config = Config.from_ini_file(args.config)
+    config.dry_run = config.dry_run or args.dry_run
+
+    print(config)
 
     if not test_online():
-        logger.critical("not online exiting")
+        logger.critical("not online, exiting")
         sys.exit(-1)
 
     summary = {}
-    for sd in (Path("/etc/").expanduser().resolve(),):  # SOURCE_DIRS_PATH.iterdir():
+    for sd in config.source_path_dirs.iterdir():
         if not sd.is_dir():
             logger.info(f"Skipping {sd}")
             continue
@@ -292,14 +335,24 @@ def main():
         else:
             summary[sd]["check"] = check(sd)
 
-    logger.info("")
-    logger.info("Summary:")
+    logger.info(summary_to_str(summary))
+
+    email_notify(summary)
+
+    if not successful(summary):
+        sys.exit(1)
+
+
+def summary_to_str(summary):
+    retval = "Summary:"
     for sd, results in summary.items():
-        logger.info(f"  {sd.as_posix()}:")
+        retval += f"\n  {sd.as_posix()}:"
         for step_name, step_result in results.items():
-            logger.info(f"    {step_name:6}: {'OK'if step_result else 'FAILED'}")
-    logger.info("")
-    logger.info("See individual logfiles for more info")
+            retval += f"\n    {step_name:6}: {'OK' if step_result else 'FAILED'}"
+    retval += "\n"
+    retval += "\nSee individual logfiles for more info"
+
+    return retval
 
 
 if __name__ == "__main__":

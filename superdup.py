c3ccff6cad692bbd2455dbe14f9dff94626b5e3b
Kilian Lackhove <kilian@lackhove.de>
2020-11-15 14:04:33 +0100

    added config parser

--- superdup.py
+++ superdup.py
@@ -8,24 +8,56 @@ import logging
 import re
 import subprocess
 import sys
+from configparser import ConfigParser
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import List, Mapping
-
-DUPLICACY_COMMAND = "/usr/bin/duplicacy"
-STAMP_PATH = Path("~/.duplicacy_stamps_new").expanduser().resolve()
-SOURCE_DIRS_PATH = Path("/source_dirs/")
-DRY_RUN = True
-
-DUPLICACY_ENV = {
-    "DUPLICACY_PASSWORD": "topsecret",
-    "DUPLICACY_B2_ID": "secret",
-    "DUPLICACY_B2_KEY": "supersecret",
-}
-
+from typing import List
 
 logger = logging.getLogger("superdup")
 logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+@dataclass
+class Config:
+    duplicacy_command: str = "/usr/bin/duplicacy"
+    stamp_path: Path = Path("~/.duplicacy_stamps_new").expanduser().resolve()
+    source_path_dirs: Path = Path("/source_dirs/")
+    dry_run: bool = True
+    duplicacy_env = {
+        "DUPLICACY_PASSWORD": "topsecret",
+        "DUPLICACY_B2_ID": "secret",
+        "DUPLICACY_B2_KEY": "supersecret",
+    }
+
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super(Config, cls).__new__(cls)
+        return cls.__instance
+
+    def from_ini_file(self, path: Path):
+        parser = ConfigParser()
+        # enable case sensitive keys
+        parser.optionxform = lambda option: option
+        parser.read(path)
+        kwargs = dict(parser["superdup"])
+        # TODO: use attrs so we can have converters
+        self.duplicacy_command = str(
+            kwargs.get("duplicacy_command", self.duplicacy_command)
+        )
+        self.stamp_path = (
+            Path(kwargs.get("stamp_path", self.stamp_path)).expanduser().resolve()
+        )
+        self.source_path_dirs = (
+            Path(kwargs.get("source_path_dirs", self.source_path_dirs))
+            .expanduser()
+            .resolve()
+        )
+
+        envs = dict(parser["dulicacy-env"])
+        self.duplicacy_env.update(envs)
 
 
 class NetworkError(Exception):
@@ -33,20 +65,28 @@ class NetworkError(Exception):
 
 
 def test_online():
+    # TODO: implement this
     return True
 
 
-def call_duplicacy(command: List[str], cwd: Path, dry_run=DRY_RUN):
-    command.insert(0, DUPLICACY_COMMAND)
+def call_duplicacy(command: List[str], cwd: Path, dry_run=None):
+    config = Config()
+    if dry_run is None:
+        dry_run = config.dry_run
+
+    command.insert(0, config.duplicacy_command)
     if logger.isEnabledFor(logging.DEBUG):
         command.insert(1, "-debug")
-    logger.debug(f"runnig command {' '.join(command)}")
+    if dry_run:
+        logger.debug(f"would run command '{' '.join(command)}' (dry-run)")
+    else:
+        logger.debug(f"runnig command '{' '.join(command)}'")
     if not dry_run:
         p = subprocess.Popen(
             command,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            env=DUPLICACY_ENV,
+            env=config.duplicacy_env,
             cwd=cwd,
             text=True,
         )
@@ -100,8 +140,10 @@ def prune(source_dir):
 
 
 def load_stamps():
+    config = Config()
+
     try:
-        with open(STAMP_PATH, "r") as f:
+        with open(config.stamp_path, "r") as f:
             stamps = json.load(f)
     except IOError:
         stamps = {}
@@ -109,37 +151,26 @@ def load_stamps():
 
 
 def save_stamp(source_dir):
+    config = Config()
+
     stamps = load_stamps()
     stamps[source_dir.as_posix()] = datetime.now().isoformat()
-    STAMP_PATH.parent.mkdir(parents=True, exist_ok=True)
-    with open(STAMP_PATH, "w") as f:
+    config.stamp_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(config.stamp_path, "w") as f:
         json.dump(stamps, f)
 
 
 def verify(source_dir):
     logger.info(f"starting verification for {source_dir}")
 
-    # p = subprocess.Popen(
-    #     [DUPLICACY_COMMAND, "list"],
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     env=DUPLICACY_ENV,
-    #     cwd=source_dir,
-    # )
-    # out, err = p.communicate()
-    # if p.returncode != 0:
-    #     logger.error(f"list failed for {source_dir}")
-    #     return False
-
-    out, _ = call_duplicacy(["list",], cwd=source_dir, dry_run=False)
-    print(out)
+    out, _ = call_duplicacy(["list"], cwd=source_dir, dry_run=False)
     matches = re.findall(r"Snapshot (\S+) revision (\d+)", out)
     snapshot_id, latest_rev = matches[-1]
 
     logger.info(f"verifying snapshot {snapshot_id} revision {latest_rev}")
     try:
         call_duplicacy(
-            ["check", "-chunks", "-r", latest_rev, "-id", snapshot_id,], cwd=source_dir,
+            ["check", "-chunks", "-r", latest_rev, "-id", snapshot_id], cwd=source_dir,
         )
     except subprocess.CalledProcessError:
         logger.error(f"verification failed for {source_dir}")
@@ -152,8 +183,10 @@ def verify(source_dir):
 
 def check(source_dir):
     logger.info(f"starting check for {source_dir}")
+    config = Config()
+
     try:
-        call_duplicacy([DUPLICACY_COMMAND, "check"], cwd=source_dir)
+        call_duplicacy([config.duplicacy_command, "check"], cwd=source_dir)
     except subprocess.CalledProcessError:
         logger.error(f"ERROR: check failed for {source_dir}")
         return False
@@ -180,7 +213,13 @@ def main():
         description="perform duplicacy backups, prune and checks"
     )
     parser.add_argument("--verbosity", type=int, choices=range(5))
+    parser.add_argument(
+        "--config",
+        type=lambda x: Path(x).expanduser().resolve(),
+        default=Path("config.ini").resolve(),
+    )
     parser.add_argument("--force-verification", action="store_true")
+    parser.add_argument("--dry-run", action="store_true")
 
     args = parser.parse_args()
 
@@ -193,6 +232,10 @@ def main():
             4: logging.DEBUG,
         }.get(args.verbosity)
     )
+
+    config = Config()
+    config.from_ini_file(args.config)
+    config.dry_run = args.dry_run
 
     if not test_online():
         logger.critical("not online exiting", flush=True)
